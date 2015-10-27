ececb3c4341e7d0f75088f6f771b733abbd35b23
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-10-27 17:30:58 -0400

    new lab

---
+++ lib/rescue_princess_peach.rb
@@ -0,0 +1,16 @@
+def rescue_princess_peach
+  status = "rescued"
+  puts "Hooray! Mario has rescued Princess Peach."
+end
+
+
+
+
+## Comment the below back in to see method scope in action!
+
+# rescue_princess_peach
+# puts status
+
+
+
+
