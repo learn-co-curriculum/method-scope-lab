198abe6f942431a3b40aa39aba3eea633238c012
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-21 17:47:10 -0500

    commit tree fa39f1553c3d12dde5333b6cb54a56beb260bc85

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,4 @@
-def catch_phrase
+def catch_phrase(phrase)
   puts phrase
 end
+
--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -5,5 +5,5 @@ end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+#rescue_princess_peach
+#puts status
